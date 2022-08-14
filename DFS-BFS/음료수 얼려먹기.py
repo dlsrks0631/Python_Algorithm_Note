3ef601bd2f8a6202a56e7429d1407e9d4ba71f9a
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-14 22:18:52 +0900

    Update 음료수 얼려먹기.py

--- DFS-BFS/음료수 얼려먹기.py
+++ DFS-BFS/음료수 얼려먹기.py
@@ -8,9 +8,9 @@ ex) 0 0 1 1 0     --> 3개
     1 1 1 1 1
     0 0 0 0 0
 
-< 문제 해결 >
-(0,0) 부터 시작해서 상 하 좌 우를 확인하며 방문하지 않았으며 구멍이 뚫려 있으면 방문처리
-그리고 상 하 좌 우를 확인했는데 방문을 한 것이거나 칸막이가 존재하지 않는 경우 결과값을 1씩 더하자
+               (x-1,y)
+     (x,y-1)    (x,y)  (x,y+1)
+               (x+1,y)
 '''
 
 import sys
@@ -20,7 +20,6 @@ input = sys.stdin.readline
 n,m = map(int,input().split())    # 세로길이 n과 가로길이 m을 입력 받음
 
 graph = []
-
 # 그래프 입력받기
 for i in range(n):
     graph.append(list(map(int,input().rstrip())))
@@ -38,5 +37,29 @@ strip()은 공백을 없앰
 
 visited = [[False] * m for _ in range(n)]
 
-print(graph)
-print(visited)
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드 방문
+def dfs(x,y):
+    # 주어진 범위를 벗어나는 경우 False
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+
+        dfs(x-1,y)
+        dfs(x,y-1)
+        dfs(x+1,y)
+        dfs(x,y+1)
+        return True
+
+    return False
+
+result = 0
+
+for x in range(n):
+    for y in range(m):
+            if dfs(x,y) == True:
+                result += 1
+
+print(result)
