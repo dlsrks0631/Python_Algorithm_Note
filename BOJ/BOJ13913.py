b619b2595d1d5a6012ffd0c479d3c6acd77a75aa
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-23 15:34:01 +0900

    Create BOJ13913.py

---
+++ BOJ/BOJ13913.py
@@ -0,0 +1,45 @@
+'''
+숨바꼭질4
+
+join
+
+'''
+
+from collections import deque
+
+n, k = map(int, input().split())
+visited = [0] * 100001
+check = [0] * 100001
+
+
+def move(now):
+    data = []
+    temp = now
+
+    for _ in range(visited[now] + 1):
+        data.append(temp)
+        temp = check[temp]
+    print(' '.join(map(str, data[::-1])))
+
+
+def bfs():
+    queue = deque([n])
+
+    while queue:
+        now = queue.popleft()
+
+        if now == k:
+            print(visited[now])
+
+            # 어떻게 움직였는지 출력
+            move(now)
+            return  # 빠져나옴
+
+        for next_node in (now - 1, now + 1, now * 2):
+            if 0 <= next_node <= 100000 and visited[next_node] == 0:
+                visited[next_node] = visited[now] + 1
+                queue.append(next_node)
+                check[next_node] = now
+
+
+bfs()
