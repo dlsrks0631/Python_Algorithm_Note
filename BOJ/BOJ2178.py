c45d8d32671b42e93db3e347c01d12cc474ba8b6
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-09-25 00:04:29 +0900

    BOJ2178

---
+++ BOJ/BOJ2178.py
@@ -0,0 +1,48 @@
+'''
+BOJ 2178번 미로탐색
+
+미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, 
+(1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 
+한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
+'''
+
+
+from collections import deque
+
+n,m = map(int,input().split())
+
+data = []
+
+for _ in range(n):
+    data.append(list(map(int,input().split())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x,y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 1:
+                queue.append((nx,ny))
+                data[nx][ny] = data[x][y] + 1
+
+
+    return data[n-1][m-1]
+
+print(bfs(0,0))
+
+
+
+    
+
+
+
